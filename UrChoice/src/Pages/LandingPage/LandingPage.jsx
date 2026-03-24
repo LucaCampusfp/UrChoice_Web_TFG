006dea64b336b5fb017e1e4918655a7842708108
LordGrim551 <lucacampusfp@gmail.com>
2026-03-24 22:10:52 +0100

    ahora bien

--- UrChoice/src/Pages/LandingPage/LandingPage.jsx
+++ UrChoice/src/Pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Helmet } from "react-helmet-async";
 import Features from "../../Components/Features";
 import LandingHeader from "../../Components/LandingHeader";
 import HeroSection from "../../Components/HeroSection";
@@ -12,7 +12,13 @@ import '../LandingPage/LandingPage.css';
 function LandingPage() {
     return (
         <>
-            
+            <Helmet>
+                <title>UrChoice - Juego de cartas multijugador online</title>
+                <meta
+                    name="description"
+                    content="UrChoice: juego de cartas multijugador online. Crea tu mazo, juega con amigos y descubre cartas populares."
+                />
+            </Helmet>
 
             <LandingHeader />
             <HeroSection />
